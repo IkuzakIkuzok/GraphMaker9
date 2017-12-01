1653f1abcd865e8e0d7b66823f4458c6a9e64b61
IkuzakIkuzok <k.kohzuki@gmail.com>
2017-12-01 21:36:02 +0900

    Init Electron

---
+++ index.js
@@ -0,0 +1,22 @@
+
+/* (c) 2017 Kazuki KOHZUKI */
+
+const {app, BrowserWindow} = require('electron');
+const dialog = require('electron').dialog;
+const path = require('path');
+
+var win = null
+
+app.on('ready', function() {
+  win = new BrowserWindow({width: 800, height: 600})
+  win.loadURL(`file://${__dirname}/index.html`)
+  win.on('closed', () => {
+    mainWindow = null
+  })
+})
+
+app.on('window-all-closed', function () {
+if (process.platform !== 'darwin') {
+  app.quit()
+}
+})
