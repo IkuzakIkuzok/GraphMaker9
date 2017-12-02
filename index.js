f5b048ee94fa03e167c561004302ccc681394675
IkuzakIkuzok <k.kohzuki@gmail.com>
2017-12-02 20:39:55 +0900

    Add error handler and so on

--- index.js
+++ index.js
@@ -1,22 +1,50 @@
 
 /* (c) 2017 Kazuki KOHZUKI */
 
-const {app, BrowserWindow} = require('electron');
-const dialog = require('electron').dialog;
-const path = require('path');
+const {app, BrowserWindow} = require('electron')
+const dialog = require('electron').dialog
+const path = require('path')
 
 var win = null
 
 app.on('ready', function() {
-  win = new BrowserWindow({width: 800, height: 700})
-  win.loadURL(`file://${__dirname}/index.html`)
+  win = new BrowserWindow({width: 800, height: 700, show: fasle})
+
+  win.on('unresponsive', () => {
+    const options = {
+      type: 'warning'
+      buttons: ['Force Close', 'Keep Waiting']
+      message: 'App is not responding'
+      detail: 'The editor is not responding. Would you like to force close it or just keep waiting?'
+    }
+    dialog.showMessageBox(options, (index) => {
+      if (index === 0) win.reload()
+      else win.destroy()
+    })
+  })
+  win.webContents.on('crashed' () => {
+    const options = {
+      type: 'warning'
+        buttons: ['Close Window', 'Reload', 'Keep It Open']
+        message: 'The app has crashed'
+        detail: 'Please report this issue to https://twitter.com/ikuzak_ikuzok'
+    }
+    dialog.showMessageBox(options, (index) => {
+      if (index === 0) win.destroy()
+      else if (index === 1) win.reload()
+    })
+  })
   win.on('closed', () => {
     mainWindow = null
   })
+  win.once('ready-to-show', () => {
+    win.show()
+  })
+  win.loadURL(`file://${__dirname}/index.html`)
 })
 
 app.on('window-all-closed', function () {
-if (process.platform !== 'darwin') {
-  app.quit()
-}
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
